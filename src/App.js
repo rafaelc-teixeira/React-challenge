420a7db72c811d094e17bdafaf6ef75ff32dceec
rafaelRato <rafaelcteixeira1@hotmail.com>
2021-05-16 14:52:28 -0300

    first design of create-book page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [ bookName, setBookName] = useState ("");
+  const [ author, setAuthor] = useState ("");
+  const [ description, setDescription] = useState ("");
+  const [ image, setImage] = useState ("");
+
+  const submitBook = () => {
+    Axios.post('http://localhost:3001/api/v1/books', {
+      name: bookName,
+      author: author,
+      description: description,
+      image: image,
+    }).then( () => {
+      alert("Deu certo porra");
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add a new book</h1>
+
+      <div className="insertBookForm">
+        <label>Name:</label>
+        <input
+          type="text"
+          name="bookName"
+          onChange={(e) => {
+            setBookName(e.target.value);
+          }}
+        />
+
+
+        <label>Author:</label>
+        <input
+          type="text"
+          name="author"
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        />
+
+
+        <label>Description:</label>
+        <input
+          type="text"
+          name="description"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
+
+
+        <label>Image:</label>
+        <input
+          type="text"
+          name="image"
+          onChange={(e) => {
+            setImage(e.target.value);
+          }}
+        />
+
+        <button onClick={submitBook}>Add new book</button>
+      </div>    
     </div>
   );
 }
